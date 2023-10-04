2ffb7310ca1f1da8be6cf7a2d00de69b61cb5fce
Gabriel Fleury <fleurygabriel000@gmail.com>
2023-10-04 13:32:50 +0200

    initialisation

---
+++ src/main/java/fr/fleury/LivreCuisineApplication.java
@@ -0,0 +1,13 @@
+package fr.fleury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivreCuisineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivreCuisineApplication.class, args);
+	}
+
+}
